7b5734709c7a0d9a3d576594ba5e9410e0dd735c
Ramiro Gutierrez <ramiro.a.gutierrez@gmail.com>
2016-04-17 23:20:13 -0300

    Ejercicio 1.a funcionando correctamente

--- Ejercicio1/Codigo.c
+++ Ejercicio1/Codigo.c
@@ -7,6 +7,7 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
 /*
 	*   Retorna un String nuevo que es la concatenación
@@ -17,18 +18,35 @@
 	*   =>
 	*   saludo = "Hola Ritchie"
 	*/
-	char* string_concat(char* cadena1[], char* cadena2[]){
 
-		char* result;
-		result = strcat(cadena1, cadena2);
-		return result;
-	}
+//Declaración de la funciones
+char *string_concat(char *s,char *t);
 
+//Programa principal
 int main(int argc, char** argv){
-	char* cadena1 = "Hola ";
-	char* cadena2 = "guti";
 
-	printf(string_concat(cadena1, cadena2));
+	char *cadena1="Hola ";
+	char *cadena2="mundo!";
+
+	char *resultado = string_concat(cadena1,cadena2);
+	printf("cat: %s\n", resultado);               /* 2: you can use %s to print a string */
+	free(resultado);
 
 	return EXIT_SUCCESS;
+}
+
+
+char *string_concat(char *start,char *end)
+{
+    char *result = malloc(strlen(start)+strlen(end)+1);    /* 3: you will have to reserve memory to hold the copy. */
+    int ptr =0, temp = 0;                   /* 4 initialise some helpers */
+
+    while(start[temp]!='\0'){                  /* 5. use the temp to "walk" over string 1 */
+        result[ptr++] = start[temp++];
+    }
+    temp=0;
+    while(end[temp]!='\0'){                   /* and string two */
+        result[ptr++]=end[temp++];
+    }
+    return result;
 }
